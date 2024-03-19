5efa62323bd7409da20e92d45ab72f40312799ef
ikinoti <gitongakinoti1@gmail.com>
2024-03-19 14:31:15 +0300

    modified

--- crm/settings.py
+++ crm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 import environ
 
 from pathlib import Path
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY =env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-ytx)0)&3btg@_+!3*^lrd=9%6e(rl71of6(9=fs(9=q&l67=u='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG =env('DEBUG')
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -123,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
