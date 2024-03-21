3ddbd299c8796fc7d57cf76c571fa377e8e7cc34
ikinoti <gitongakinoti1@gmail.com>
2024-03-21 19:33:11 +0300

    modified our functions to render data from model to templates

--- account/views.py
+++ account/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'account/dashboard.html', context)
+
+def products(request):
+    products = Product.objects.all()
+
+    return render(request, 'account/products.html', {'products':products})
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+
+    return render(request, 'account/customer.html', context)
