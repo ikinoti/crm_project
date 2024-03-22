3b4e996ec042b1c13923cf7250e5a644e921a5ee
ikinoti <gitongakinoti1@gmail.com>
2024-03-22 17:33:08 +0300

    implemented create, update and delete order functionality

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 def home(request):
@@ -34,3 +35,39 @@ def customer(request, pk):
     context = {'customer': customer, 'orders': orders, 'order_count': order_count}
 
     return render(request, 'account/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print('PRIRNTING POST: ', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'account/order_form.html', context)
+
+def updateOrder(request, pk):
+    order =Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        # print('PRIRNTING POST: ', request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'account/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order =Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'order': order}
+    return render(request, 'account/delete.html', context)
+ 
